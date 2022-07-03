dcf1004379ff0c2cca7682536cb0db3934862f82
Valeriy Kirichenko <vmkirichenko1991@yandex.ru>
2022-07-03 23:13:07 +0300

    First commit

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -110,9 +110,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [BASE_DIR + '/api/static/']
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "/api/static/"),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
