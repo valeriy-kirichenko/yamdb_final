fa2965247b3b756e2fd401070e65cf1c3d282be5
Valeriy Kirichenko <vmkirichenko1991@yandex.ru>
2022-07-06 14:51:52 +0300

    fixed static

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(find_dotenv())
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
