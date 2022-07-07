dd7d9f6d369906b844b1a2580c458485c7e014ec
Valeriy Kirichenko <vmkirichenko1991@yandex.ru>
2022-07-07 15:26:30 +0300

    fixed static

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR + '/api/static/']
-STATIC_ROOT = [BASE_DIR + '/api/static/']
+STATIC_ROOT = [BASE_DIR + '/static/']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
